1e1f21eb1f2a79c5cba7fb39df31c709f2dc7c21
吕自强 <lzq8239@163.com>
2017-12-20 09:40:48 +0800

    add

--- src/main/java/com/greentown/learn/cloud/HelloResource.java
+++ src/main/java/com/greentown/learn/cloud/HelloResource.java
@@ -1,6 +1,7 @@
 package com.greentown.learn.cloud;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +20,32 @@ public class HelloResource {
 
     @HystrixCommand(fallbackMethod = "fallback", groupKey = "Hello",
             commandKey = "hello",
-            threadPoolKey = "helloThread"
+            threadPoolKey = "helloThread",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000"),//指定多久超时，单位毫秒。超时进fallback
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),//判断熔断的最少请求数，默认是10；只有在一个统计窗口内处理的请求数量达到这个阈值，才会进行熔断与否的判断
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "10"),//判断熔断的阈值，默认值50，表示在一个统计窗口内有50%的请求处理失败，会触发熔断
+                },
+                threadPoolProperties = {
+                        @HystrixProperty(name = "coreSize", value = "30"),
+                        @HystrixProperty(name = "maxQueueSize", value = "101"),
+                        @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+                        @HystrixProperty(name = "queueSizeRejectionThreshold", value = "15"),
+                        @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "12"),
+                        @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "1440")
+            }
+
             )
     @GetMapping
     public String hello() {
-        String url = "http://hello-server/rest/hello/server";
+        String url = "http://MYAPP/rest/sms/hello";
         return restTemplate.getForObject(url, String.class)+ " including client";
     }
     
     
     @RequestMapping(value="/sendAuthCode",method = RequestMethod.POST)
     public boolean sendAuthCode() {
-        String url = "http://MYAPP/api/sms/sendAuthCode";
+        String url = "http://MYAPP/rest/sms/sendAuthCode";
         SendAuthCodeRO obj=new SendAuthCodeRO();
         obj.setMobile("18768439095");
         obj.setBizNo("cloud");
